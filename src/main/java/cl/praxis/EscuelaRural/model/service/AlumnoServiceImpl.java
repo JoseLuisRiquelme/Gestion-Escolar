37dc99da3c4f9eff44febacf29bfb80d1aa02b88
Jose Luis Riquelme <jlrsaez@gmail.com>
2024-08-12 17:46:14 -0400

    Adding @Transactional to the project to got ACID transactions

--- src/main/java/cl/praxis/EscuelaRural/model/service/AlumnoServiceImpl.java
+++ src/main/java/cl/praxis/EscuelaRural/model/service/AlumnoServiceImpl.java
@@ -3,6 +3,7 @@ package cl.praxis.EscuelaRural.model.service;
 import cl.praxis.EscuelaRural.model.entities.Alumno;
 import cl.praxis.EscuelaRural.model.repositories.AlumnoRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 @Service
@@ -12,28 +13,29 @@ public class AlumnoServiceImpl implements AlumnoService {
     public AlumnoServiceImpl(AlumnoRepository aRepository){
         this.aRepository=aRepository;
     }
+    @Transactional
     @Override
     public List<Alumno> findAll() {
         return aRepository.findAll();
     }
-
+    @Transactional
     @Override
     public Alumno findOne(int id) {
         return aRepository.findById(id).orElse(null);
     }
-
+    @Transactional
     @Override
     public boolean create(Alumno a) {
         Alumno result = aRepository.save(a);
         return result!=null;
     }
-
+    @Transactional
     @Override
     public boolean update(Alumno a) {
         Alumno result = aRepository.save(a);
         return result!=null;
     }
-
+    @Transactional
     @Override
     public boolean delete(int id) {
         boolean exist = aRepository.existsById(id);
--- src/main/java/cl/praxis/EscuelaRural/model/service/CursoServiceImpl.java
+++ src/main/java/cl/praxis/EscuelaRural/model/service/CursoServiceImpl.java
@@ -3,6 +3,7 @@ package cl.praxis.EscuelaRural.model.service;
 import cl.praxis.EscuelaRural.model.entities.Curso;
 import cl.praxis.EscuelaRural.model.repositories.CursoRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 @Service
@@ -13,30 +14,30 @@ CursoRepository cRepository;
 public CursoServiceImpl(CursoRepository cRepository){
     this.cRepository=cRepository;
 }
-
+    @Transactional
     @Override
     public List<Curso> findAll() {
         return cRepository.findAll();
     }
-
+    @Transactional
     @Override
     public Curso findOne(int id) {
 
     return cRepository.findById(id).orElse(null);
     }
-
+    @Transactional
     @Override
     public boolean create(Curso c) {
     Curso result = cRepository.save(c);
         return result!=null;
     }
-
+    @Transactional
     @Override
     public boolean update(Curso c) {
     Curso result=cRepository.save(c);
         return result!=null;
     }
-
+    @Transactional
     @Override
     public boolean delete(int id) {
     boolean exist = cRepository.existsById(id);
